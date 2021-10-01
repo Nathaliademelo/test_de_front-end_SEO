8f498806a6697b48554a67d7947f92abefca817b
Nathaliademelo <88354520+Nathaliademelo@users.noreply.github.com>
2021-10-01 00:11:31 -0300

    Arquivos do teste
    Envio de arquivos do teste de front-end

---
+++ Teste de front-end SEO/script.js
@@ -0,0 +1,50 @@
+    function show(shown, hidden) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden).style.display='none';
+    return false;
+    }
+    
+    function Adicionar(produto, qtd, valor, posicao) {
+		localStorage.setItem("produto" + posicao, produto);
+		localStorage.setItem("qtd" + posicao, qtd);
+		valor = valor * qtd;
+		localStorage.setItem("valor" + posicao, valor);
+		alert("Produto adicionado ao carrinho!");  
+    }
+
+    function validaFormulario(){
+
+        if(document.getElementById("nome").value != "" &&
+           document.getElementById("email").value != "" &&
+           document.getElementById("telefone").value != "" ){
+      
+          alert("Agradecemos o seu interesse!! Você receberá as novidades por e-mail!!")
+        }else{
+          alert("Por favor, preencha os campos nome e email.")
+        }
+    }
+
+    function exibeValorCarrinho(){
+        var total = 0; // variável que retorna o total dos produtos que estão na LocalStorage.
+        var i = 0;     // variável que irá percorrer as posições
+        var valor = 0; // variável que irá receber o preço do produto convertido em Float.
+        
+        for(i=1; i<=99; i++) // verifica até 99 produtos registrados na localStorage
+        {
+            var prod = localStorage.getItem("produto" + i + ""); // verifica se há recheio nesta posição. 
+            if(prod != null) 
+            {	
+                // exibe os dados da lista dentro da div itens
+                document.getElementById("itens").innerHTML += localStorage.getItem("qtd" + i) + " x ";
+                document.getElementById("itens").innerHTML += localStorage.getItem("produto" + i);
+                document.getElementById("itens").innerHTML += " ";
+                document.getElementById("itens").innerHTML += "R$: " + localStorage.getItem("valor" + i) + "<hr>";
+                
+                // calcula o total dos recheios
+                valor = parseFloat(localStorage.getItem("valor" + i)); // valor convertido com o parseFloat()
+                total = (total + valor); // arredonda para 2 casas decimais com o .toFixed(2)
+            }
+        } 
+        // exibe o total dos recheios
+        document.getElementById("total").innerHTML = total.toFixed(2); 
+    }
